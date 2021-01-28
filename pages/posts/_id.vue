b8acf14ff5d803f7d9282f757b4689e0b3f4d67e
Maxim Antonuk <maxim.antonuk@gmail.com>
2021-01-28 19:24:48 +0200

    Posts

---
+++ pages/posts/_id.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bg-light p-4 rounded mt-4 border border-secondary">
+    <div>
+      <h2 class="title">{{ post.title }}</h2>
+      <hr />
+      <div>{{ post.body }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  validate({ params }) {
+    return /^\d+$/.test(params.id);
+  },
+  async asyncData({ $axios, params }) {
+    const post = await $axios.$get(
+      `https://jsonplaceholder.typicode.com/posts/${params.id}`
+    );
+    return { post };
+  }
+};
+</script>
+
+<style></style>
